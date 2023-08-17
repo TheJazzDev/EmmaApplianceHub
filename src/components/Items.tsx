b9bd822b0ab6533747fb6d19f10562183a5a66f6
TheJazzDev <babsman4all@gmail.com>
2023-08-17 16:19:48 +0100

    add more items

--- src/components/Items.tsx
+++ src/components/Items.tsx
@@ -5,7 +5,7 @@ const Items = () => {
   return (
     <div className='flex flex-col lg:flex-row gap-20 items-center justify-center flex-wrap'>
       {ItemsArray.map((item) => (
-        <div key={item.id} className='max-w-lg bg-card rounded-2xl'>
+        <div key={item.id} className='max-w-lg bg-card rounded-2xl relative'>
           <Image
             src={item.img}
             alt={item.title}
@@ -16,6 +16,14 @@ const Items = () => {
             <h2 className='text-100 text-2xl'>{item.title}</h2>
             <p className='text-card-text mt-2'>{item.desc}</p>
           </div>
+          {item.sold && (
+            <Image
+              src={item.sold}
+              alt='Sold'
+              priority={item.priority}
+              className='w-60 absolute -top-10'
+            />
+          )}
         </div>
       ))}
     </div>
--- src/components/ItemsArray.ts
+++ src/components/ItemsArray.ts
@@ -1,13 +1,23 @@
 import chairs from '../../public/asset/chairs.jpg';
-import fridge from '../../public/asset/fridge.jpg';
+import fridge_close from '../../public/asset/fridge_close.jpg';
 import dinning_set from '../../public/asset/dinning_set.jpg';
 import television from '../../public/asset/television.jpg';
 import washing from '../../public/asset/washing_machine.jpg';
+import sit_up from '../../public/asset/sit_up.jpg';
+import mattress from '../../public/asset/mattress.jpg';
+import side_drawer1 from '../../public/asset/side_drawer1.jpg';
+import side_drawer2 from '../../public/asset/side_drawer2.jpg';
+import dress_mirror from '../../public/asset/dress_mirror.jpg';
+import bed_frame from '../../public/asset/bed_frame.jpg';
+import fridge_open from '../../public/asset/fridge_open.jpg';
+import generator from '../../public/asset/generator.jpg';
+import sold from '../../public/asset/sold.webp';
 
 interface Item {
   id: number;
   title: string;
   img: any;
+  sold?: any;
   desc: string;
   priority?: boolean;
 }
@@ -15,36 +25,85 @@ interface Item {
 const ItemsArray: Item[] = [
   {
     id: Math.random(),
-    title: 'Chairs',
+    title: 'Sitting Chairs',
     img: chairs,
-    desc: 'This is a full set of chairs',
-    priority: true
-  },
-  {
-    id: Math.random(),
-    title: 'Television',
-    img: television,
-    desc: 'This is a Television',
-    priority: true
+    desc: 'Full Set of Sitting Chairs (3, 2, 1 Seater)',
+    priority: true,
   },
   {
     id: Math.random(),
     title: 'Dining Chairs',
     img: dinning_set,
-    desc: 'This is a full set of chairs',
-    priority: true
+    desc: '4 Seater Dinning Set',
+    priority: true,
+  },
+  {
+    id: Math.random(),
+    title: 'Television',
+    img: television,
+    desc: '55 inches Hisense Curve TV',
+    priority: true,
+    sold: sold,
   },
   {
     id: Math.random(),
     title: 'Washing Machine',
     img: washing,
-    desc: 'This is an automatic washing machine',
+    desc: 'Nexus 12.0kg Automatic Washing Machine (Soak, Wash, Rinse & Spin)',
+  },
+  {
+    id: Math.random(),
+    title: 'Generator',
+    img: generator,
+    desc: '22 Litters INC 2500 Petrol Generator',
+  },
+  {
+    id: Math.random(),
+    title: 'Sit Up Bench',
+    img: sit_up,
+    desc: 'Sit Up Bench for Workout',
+  },
+  {
+    id: Math.random(),
+    title: 'Bed Frame',
+    img: mattress,
+    desc: '6 x 6 Bed Frame (Without Back Rest)',
+  },
+  {
+    id: Math.random(),
+    title: 'Bed Frame',
+    img: bed_frame,
+    desc: '6 x 6 Bed Frame (With Back Rest)',
+  },
+  {
+    id: Math.random(),
+    title: 'Dressing Mirror',
+    img: dress_mirror,
+    desc: 'Dressing Mirror + 3 Locker and Dressing Stool',
+  },
+  {
+    id: Math.random(),
+    title: 'Side Drawer 1',
+    img: side_drawer1,
+    desc: 'Bed Frame Side Drawer 1',
+  },
+  {
+    id: Math.random(),
+    title: 'Side Drawer 2',
+    img: side_drawer2,
+    desc: 'Bed Frame Side Drawer 2',
   },
   {
     id: Math.random(),
     title: 'Hisense Fridge',
-    img: fridge,
-    desc: 'This is a fridge',
+    img: fridge_open,
+    desc: '262 L Volume Hisense Fridge + Freezer (Inside View)',
+  },
+  {
+    id: Math.random(),
+    title: 'Hisense Fridge',
+    img: fridge_close,
+    desc: '262 L Volume Hisense Fridge + Freezer (Outside View)',
   },
 ];
 
